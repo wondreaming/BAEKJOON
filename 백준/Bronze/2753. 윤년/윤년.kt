494106e72c0f52eecd70b8df5f03dc9c426defad
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-14 17:16:47 +0900

    [Bronze V] Title: 윤년, Time: 80 ms, Memory: 12324 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.kt
+++ 백준/Bronze/2753. 윤년/윤년.kt
@@ -1,6 +1,9 @@
 fun main() {
-    val year = readln().trim().toInt()
+    val year = readln().toInt()
 
-    val result = if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) 1 else 0
-    println(result)
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+        println(1)
+    } else {
+        println(0)
+    }
 }
